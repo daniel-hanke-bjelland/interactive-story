d119355b530bfde5cf0dd63ccba0f0f209192779
Daniel Hanke Bjelland <danielhankebjelland@Mac.home>
2026-03-19 18:14:57 +0100

    Ryddet opp og slettet undøvendig kode i oppretKonto funksjonen

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 function opppretKonto() {
-  // console.log("PRØVER Å OPPRETTE EN KONTO");
 
   let navn = document.getElementById("name_sign_up").value;
-  // console.log(navn);
-
   let epost = document.getElementById("epost_sign_up").value;
-  // console.log(epost);
-
   let nickname = document.getElementById("nickname_sign_up").value;
-  // console.log(nickname);
-
   let passord = document.getElementById("password_sign_up").value;
-  // console.log(passord);
 
   if (navn !== "" && epost !== "" && nickname !== "" && passord !== "") {
     let bruker = {
@@ -21,24 +13,16 @@ function opppretKonto() {
       passord,
     };
 
-    // console.log(bruker);
-
     localStorage.setItem("bruker", JSON.stringify(bruker));
-
-    // window.URL(/html/historien.html);
-
-    // window.location.replace.URL(/html/historien.html);
 
     window.location.replace("../html/historien.html");
   } else {
-    // console.log("DU MÅ FYLLE UT ALT");
     document.getElementById("wrapper_4_out").innerHTML =
       "You must fill in all of the fields.";
   }
 }
 
 let user = JSON.parse(localStorage.getItem("bruker"));
-console.log(user);
 
 const historie = [
   {
